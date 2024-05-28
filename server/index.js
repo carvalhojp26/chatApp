5403143a600abbf9f4d3f92bfd8794e73ac80f56
joao <acarvalho.jp@gmail.com>
2024-05-28 10:33:28 -0400

    Adding sockets to test basic backend-frontend connection

--- server/index.js
+++ server/index.js
@@ -1,15 +1,34 @@
 const express = require('express')
-const app = express()
-
 const mongoose = require('mongoose')
-const mongoDBURL = 'mongodb://localhost:27017/chat'
+const http = require('http');
+const socketIo =  require('socket.io');
+const cors = require('cors')
 
-mongoose.connect(mongoDBURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => { console.log("Connection Successfull") })
-    .catch((err) => { console.log("Received an Error") })
+const app = express()
+const server = http.createServer(app)
+const io = socketIo(server, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST']
+    }
+  });
+
+app.use(express.static('public'));
+
+const mongoDBURL = 'mongodb://localhost:27017/chat';
+
+(async () => {
+    try {
+        await mongoose.connect(mongoDBURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        console.log("connection sucessful")
+    } catch(error) {
+        console.error("Received an error", error)
+        process.exit(1)
+    }
+})()
 
 const Schema = mongoose.Schema
 
@@ -20,6 +39,8 @@ const mySchema = new Schema ({
 })
 
 const Message = mongoose.model('Message', mySchema)
+
+app.use(express.json());
 
 app.get('/messages', async (req, res) => {
     try {
@@ -32,10 +53,35 @@ app.get('/messages', async (req, res) => {
     }
 })
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
+app.post('/messages', async (req, res) => {
+    try {
+        const {from, to, message} = req.body
+        const newMessage = new Message({from, to, message})
+        await newMessage.save()
+        res.status(201).json(newMessage)
+    } catch(error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
 })
 
-app.listen(3000, () => {
+io.on('connection', socket => {
+    console.log('User connected')
+
+    socket.on('chat message', async message => {
+        console.log('Received message:', message);
+        const newMessage = new Message(message);
+        await newMessage.save();
+        console.log('Message saved:', newMessage);
+        io.emit('newMessage', newMessage);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected')
+    })
+})
+
+server.listen(3000, () => {
     console.log("server running on port 3000")
 })
